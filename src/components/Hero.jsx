d5e5e03274fc0e177ded203e29d777cf441157b0
A.VIKRAM <135975166+vikram662005@users.noreply.github.com>
2026-08-08 22:02:17 +0530

    Reduce delay for letter swap animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -89,7 +89,7 @@ const Hero = ({ onPreloadComplete }) => {
           );
         }
         iterations += 1 / 3; // Controls the speed of the letter swap
-      }, 180); // 50ms per step
+      }, 130); // 50ms per step
     });
 
     return () => {
