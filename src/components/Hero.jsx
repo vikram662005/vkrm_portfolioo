f60538b723d52187f0d1c19fc7b18a96c19c6103
A.VIKRAM <135975166+vikram662005@users.noreply.github.com>
2026-08-08 21:56:03 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -89,7 +89,7 @@ const Hero = ({ onPreloadComplete }) => {
           );
         }
         iterations += 1 / 3; // Controls the speed of the letter swap
-      }, 300); // 50ms per step
+      }, 240); // 50ms per step
     });
 
     return () => {
